4fba2be0aabfb6a6d72ae6e97bb9a9bf78834182
juanse254@gitlab.com <juanse254@gmail.com>
2019-03-19 15:45:23 +0100

    Added pickle serialization to send object to backend for further processing

--- pollingService.py
+++ pollingService.py
@@ -1,8 +1,16 @@
 import polling
+import pickle
+import requests
 from gitHandler import gitHandler
+
+HOST = 'http://127.0.0.1:8000/'
+PORT = 8000
+repo = None
 
 def pollGit(Repo):
     index = Repo.commit().committed_date
+    global repo
+    repo = Repo
     polling.poll(
         lambda: gitHandler.fetchData(Repo.working_dir).commit().committed_date != index, #TODO:esto verifica si hay algun commit local nuevo en comparacion al anterior(el llamado por la func).
         step=10,
@@ -12,6 +20,9 @@ def pollGit(Repo):
 
 def is_correct_response(error):
     if error == True:
+        global repo
         print('mando el commmit o diff al server')
-        
+        req = requests.post(HOST,data=pickle.dumps(repo)) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente.
+        print(req.text)
+
         #aqui hay llamar otra vez a la funcion original, cambiando el repo anterior por el nuevo para sacar nuevos cambios. o sea llamar a gitHandler con el Repo actual o devolver algo al main para que salga del loop y pueda volver a llamar.
