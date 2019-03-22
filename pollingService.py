8db38611fa740a3b558dcb2730c852eac0371cba
juanse254@gitlab.com <juanse254@gmail.com>
2019-03-22 15:59:29 +0100

    changed elements to be passed to server

--- pollingService.py
+++ pollingService.py
@@ -1,6 +1,7 @@
 import polling
 import pickle
 import requests
+import git
 from gitHandler import gitHandler
 
 HOST = 'http://127.0.0.1:8000/'
@@ -21,8 +22,10 @@ def pollGit(Repo):
 def is_correct_response(error):
     if error == True:
         global repo
+        remote_url = repo.git.remote('get-url', 'origin')
+        patch = repo.git.show()
         print('mando el commmit o diff al server')
-        req = requests.post(HOST,data=pickle.dumps(repo)) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente.
+        req = requests.post(HOST,data={'patch':patch, 'remote_url': remote_url}) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente.
         print(req.text)
 
         #aqui hay llamar otra vez a la funcion original, cambiando el repo anterior por el nuevo para sacar nuevos cambios. o sea llamar a gitHandler con el Repo actual o devolver algo al main para que salga del loop y pueda volver a llamar.
