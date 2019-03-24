b5a81dad529702f78d67ce66829857eac3a8593a
juanse254@gitlab.com <juanse254@gmail.com>
2019-03-24 12:53:42 +0100

    fix bug to allow multiple commits

--- pollingService.py
+++ pollingService.py
@@ -7,8 +7,10 @@ from gitHandler import gitHandler
 HOST = 'http://127.0.0.1:8000/'
 PORT = 8000
 repo = None
+index = None
 
 def pollGit(Repo):
+    global index
     index = Repo.commit().committed_date
     global repo
     repo = Repo
@@ -24,8 +26,10 @@ def is_correct_response(error):
         global repo
         remote_url = repo.git.remote('get-url', 'origin')
         patch = repo.git.show()
+        global index
+        index = repo.commit().committed_date
         print('mando el commmit o diff al server')
-        req = requests.post(HOST,data={'patch':patch, 'remote_url': remote_url}) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente.
-        print(req.text)
+        #req = requests.post(HOST,data={'patch':patch, 'remote_url': remote_url}) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente.
+        #print(req.text)
 
         #aqui hay llamar otra vez a la funcion original, cambiando el repo anterior por el nuevo para sacar nuevos cambios. o sea llamar a gitHandler con el Repo actual o devolver algo al main para que salga del loop y pueda volver a llamar.
