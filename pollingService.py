94c514f506ccb742b263d7423c6131214288f885
juanse254@gitlab.com <juanse254@gmail.com>
2019-04-18 22:29:11 +0200

    replaced commands using git directly with the object and decoupled helper function.

--- pollingService.py
+++ pollingService.py
@@ -9,14 +9,14 @@ HOST = 'http://127.0.0.1:8000/'
 PORT = 8000
 repo = None
 index = None
-
+#TODO:Este servicio deberia ser una clase.
 def pollGit(Repo):
     global index
     index = Repo.commit().committed_date
     global repo
     repo = Repo
     polling.poll(
-        lambda: gitHandler.fetchData(Repo.working_dir).commit().committed_date != index, #TODO:esto verifica si hay algun commit local nuevo en comparacion al anterior(el llamado por la func).
+        lambda: gitHandler.fetchData(Repo.working_dir).commit().committed_date != index,
         step=10,
         poll_forever=True,
         check_success=is_correct_response,
@@ -25,19 +25,19 @@ def pollGit(Repo):
 def is_correct_response(error):
     if error == True:
         global repo
-        remote_url = repo.git.remote('get-url', 'origin')
+        remote_url = repo.remotes.origin.url
         pivot_commit = checkDelta(remote_url)
 
         #mandar solo el delta
         delta = []
-        commitsToSend = repo.git.rev_list(pivot_commit + '..HEAD').split('\n')
+        commitsToSend = commitListtoArray(list(repo.iter_commits(pivot_commit + '..HEAD')))
         commitsToSend.reverse()
         for commit in commitsToSend:
             #patch = repo.git.show()
-            patch = repo.git.format_patch('-1', '--stdout', commit)
-            parent_commit_id = repo.git.rev_parse(commit + '~1')
+            patch = repo.git.format_patch('-1', '--stdout', commit) #TODO: revisar si esto es correcto, hay que usar diff para el merge info.
+            parent_commit_id = repo.commit(commit).parents[0].hexsha
             current_branch = repo.active_branch.name
-            parent_branch = repo.git.branch(['--contains', parent_commit_id]).replace("* ", "")
+            parent_branch = repo.git.branch(['--contains', parent_commit_id]).replace("* ", "")#TODO: mismo comentarios de 33 y 37 y 38 P.D: esto funciona?
             email = repo.commit(commit).author.email
             username = repo.commit(commit).author.name
             commit_id = repo.commit(commit).hexsha
@@ -66,27 +66,19 @@ def is_correct_response(error):
         req = requests.post(HOST, data=json.dumps(delta), headers = headers)
 
 
-            # req = requests.post(HOST,data={'patch':patch.encode(),
-            #                                'remote_url': remote_url,
-            #                                'parent_commit_id': parent_commit_id,
-            #                                'commit_id': commit_id,
-            #                                'current_branch': current_branch,
-            #                                'parent_branch': parent_branch,
-            #                                'email': email,
-            #                                'username': username,
-            #                                'message': message.encode(),
-            #                                'time': time,
-            #                                'offset': offset}) # Aqui solo mando el repo que es el padre pero puedo mandar repo.git.diff() que es el patch o lo que sea realmente        print(req.text)
-
-
 def checkDelta(remote_url):
 
     completeList= list(repo.iter_commits(repo.active_branch))
-    idList = []
-    for commit in completeList:
-        idList.append(commit.hexsha)
+    idList = commitListtoArray(completeList)
     jsonList = json.dumps({'commits' : idList, 'url' : remote_url})
     req = requests.post(HOST + 'commitCheck/', data=jsonList)
     print(req.content)
     return json.loads(req.text)['pivot_commit']
+
+def commitListtoArray(list):
+    idList = []
+    for commit in list:
+        idList.append(commit.hexsha)
+    return idList
+
         #aqu hay llamar otra vez a la funcion original, cambiando el repo anterior por el nuevo para sacar nuevos cambios. o sea llamar a gitHandler con el Repo actual o devolver algo al main para que salga del loop y pueda volver a llamar.
