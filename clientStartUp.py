b8db84239541d924f258bab1c1c37acd13d87079
Nicolas Gordillo <nicolas.gordillo@swisscom.com>
2019-05-21 17:15:25 +0200

    open svg in browser

--- clientStartUp.py
+++ clientStartUp.py
@@ -13,10 +13,11 @@ import urllib.request
 import json
 import os
 import threading
+import webbrowser
 
 result = None
 
-HOST = 'http://5a185d27.ngrok.io/'
+HOST = 'http://fa2572e4.ngrok.io/'
 
 class SvgWidget(Scatter):
 
@@ -24,7 +25,7 @@ class SvgWidget(Scatter):
         super(SvgWidget, self).__init__(**kwargs)
         with self.canvas:
             svg = Svg(filename)
-        self.size = svg.width, svg.height
+            self.size = svg.width, svg.height
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
@@ -48,24 +49,26 @@ class Root(FloatLayout):
 
     def load(self, path, filename):
         global result
-        result = gitHandler.searchGit(filename[0])
+        #result = gitHandler.searchGit(filename[0])
+        result = gitHandler.searchGit('/Users/nicolasgordillo/Documents/uzh/fs19/masterProject/git_projects/test/Coffee')
         if result:
             startHandler()
-        self.dismiss_popup()
+            self.dismiss_popup()
 
     def show_image(self):
         global result
         #repo = gitHandler.fetchData(result) #TODO:check if we selected a repo already.
         req = post(HOST + 'getGraph/', data={'gitUrl' : 'lol'} ) #pass the repo url instead of lol
         image_url = json.loads(req.text)['graphUrl']
+        webbrowser.open_new(image_url)
         if(self.svg):
             self.remove_widget(self.svg)
-        urllib.request.urlretrieve(image_url, "tmp.svg")
+        #urllib.request.urlretrieve(image_url, "tmp.svg")
         self.svg = SvgWidget('tmp.svg', size_hint=(None, None))
         self.add_widget(self.svg)
-        self.svg.scale = 1.5
+        self.svg.scale = 0.5
         self.svg.center = Window.center
-        os.remove('tmp.svg')
+        #os.remove('tmp.svg')
 
 def startHandler():
     global result
